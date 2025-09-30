6aa2a18dd89fa1805b665dcb9319e395bb4902d8
sandervancranenburgh <s.vancranenburgh@tudelft.nl>
2025-09-30 21:16:57 +0200

    inference.py

--- CVDCM/inference.py
+++ CVDCM/inference.py
@@ -12,13 +12,9 @@ from datetime import datetime
 from ImageChoicedata_preprocessing import ImageChoiceDataset
 from transformers import AutoImageProcessor
 
-# Image preprocessor
-image_processor = AutoImageProcessor.from_pretrained('facebook/deit-base-distilled-patch16-224')
-
 # Paths
 repo_root = Path(os.getcwd())
-data_dir = repo_root / "data"
-img_path = data_dir / "images"
+img_path = repo_root / "data" / "images"
 path_pretrained_model = repo_root /'CVDCM' /'pretrained_models' / 'Model3_CVDCM_CE0585_retrained.pt' # Pretrained model path
 print(path_pretrained_model)
 
@@ -29,14 +25,17 @@ print(f'Using device: {device}')
 # Load
 model = cvdcm_model(path_pretrained_model).to(device)
 
+# Image preprocessor
+image_processor = AutoImageProcessor.from_pretrained('facebook/deit-base-distilled-patch16-224')
+
 # List of images in the image folder
 img_lst = []
 for file in os.listdir(img_path):
-        if file.lower().endswith("jpg") or file.lower().endswith("png") or file.lower().endswith("jpeg"):
-            img_lst.append(file)
+    if file.lower().endswith("jpg") or file.lower().endswith("png") or file.lower().endswith("jpeg"):
+        img_lst.append(file)
 print('Number of images =', len(img_lst))
 
-# Add image path df
+# DataFrame to store results
 df = pd.DataFrame()
 df['img'] = img_lst
 
@@ -48,7 +47,7 @@ for i, imgx in enumerate(img_lst):
         print(f"Processing image {i+1}/{len(img_lst)}", end="\r")
 
         # Load image
-        img = Image.open(os.path.join(img_path, imgx)).convert('RGB')  # Always convert to RGB mode
+        img = Image.open(os.path.join(img_path, imgx)).convert('RGB') 
 
         # Preprocess image
         img = image_processor(img, return_tensors="pt").pixel_values[0].to(device)
@@ -64,6 +63,6 @@ for i, imgx in enumerate(img_lst):
         continue
 
 # Add to df
-df['V'] = V_img
+df['V_img'] = V_img
 df.to_csv(repo_root / 'image_utilities.csv', index=False)
 print(f'\nImage utilities saved to {repo_root / "image_utilities.csv"}')
